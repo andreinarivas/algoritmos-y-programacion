ebb2b0174461895d1b86db06cc737f2609f39f19
Andreina Rivas <andreina.rivas@correo.unimet.edu.ve>
2022-09-14 17:05:10 -0400

    'test'

---
+++ Semana 1/Miercoles/ejercicio1.py
@@ -0,0 +1,4 @@
+time_worked = int(input("Please enter how many hours you've worked "))
+pay_for_hour = int(input('How much you earn per hour '))
+pay = time_worked * pay_for_hour
+print(f"You've earned {pay} dollars")
