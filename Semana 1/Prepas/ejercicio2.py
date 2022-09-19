e418f713f67315d3bc1b473699fb85fe3acf1235
andreinarivas <113389360+andreinarivas@users.noreply.github.com>
2022-09-19 10:12:51 -0400

    Delete Semana 1/Prepas directory

--- Semana 1/Prepas/ejercicio2.py
+++
@@ -1,6 +0,0 @@
-chequear=input('Introduce que numero/palabra a chequear: ')
-chequear_inv=(chequear[::-1])
-if chequear==chequear_inv:
-    print(f'{chequear} es un palindromo')
-else:
-    print(f'{chequear} no es un palindromo')
